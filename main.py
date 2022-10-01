d68499313df971dce83289edfaf31cd34a03bc3b
mbenson025 <mbenson025@gmail.com>
2022-10-01 09:59:39 -0500

    random selection from array for first player and computer hands

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ player_hand = []
 dealer_hand = []
 
 
-# def deal_cards():
+def add_cards(c1, c2):
+    sum = c1 + c2
+    return sum
 
-# deal_cards()
+
+def deal_new_card():
+    """ Selects a random card value from the array 'cards' """
+    card = random.choice(cards)
+    return card
+
+
+def start_round():
+    player_hand = [deal_new_card(), deal_new_card()]
+    print(player_hand)
+    dealer_hand = [deal_new_card()]
+    print(dealer_hand)
+    print(f"Your cards: {player_hand}")
+    print(f"Dealer draws the first card: {dealer_hand}")
+    return player_hand, dealer_hand
+
+
+start_round()
