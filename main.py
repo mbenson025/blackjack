2a64202d5292b4c0c869c3f37d43889e4c9dde33
mbenson025 <mbenson025@gmail.com>
2022-10-01 10:40:58 -0500

    conditionals for draw outcomes

--- main.py
+++ main.py
@@ -9,25 +9,36 @@ player_hand = []
 dealer_hand = []
 
 
-def add_cards(c1, c2):
-    sum = c1 + c2
-    return sum
-
-
 def deal_new_card():
-    """ Selects a random card value from the array 'cards' """
+    """Selects a random card from the deck"""
     card = random.choice(cards)
     return card
 
 
-def start_round():
-    player_hand = [deal_new_card(), deal_new_card()]
-    print(player_hand)
-    dealer_hand = [deal_new_card()]
-    print(dealer_hand)
-    print(f"Your cards: {player_hand}")
-    print(f"Dealer draws the first card: {dealer_hand}")
-    return player_hand, dealer_hand
+for draw in range(2):
+    player_hand.append(deal_new_card())
+    dealer_hand.append(deal_new_card())
 
 
-start_round()
+# def start_round():
+#     player_hand = [deal_new_card(), deal_new_card()]
+#     print(player_hand)
+#     dealer_hand = [deal_new_card()]
+#     print(dealer_hand)
+#     return player_hand, dealer_hand
+# start_round()
+
+def score(current_cards):
+    if sum(current_cards) == 21 and len(current_cards) == 2:
+        print("Blackjack")
+    if sum(current_cards) > 21 and 11 in current_cards:
+        current_cards.remove(11)
+        current_cards.append(1)
+    return sum(current_cards)
+
+
+user_score = score(player_hand)
+
+print(f"Your cards: {player_hand}")
+print(f"Your score is {user_score}")
+print(f"Dealer cards: {dealer_hand}")
