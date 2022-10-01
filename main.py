4df6ba77c2223210ddf30b2829a37f0a48af47d1
mbenson025 <mbenson025@gmail.com>
2022-10-01 11:24:34 -0500

    while loop for game_over condition

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import random
 
 # 11 is ace
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+game_over = False
+
+
+def score(current_cards):
+    if sum(current_cards) == 21 and len(current_cards) == 2:
+        print("Blackjack!")
+        return 0
+    if sum(current_cards) > 21 and 11 in current_cards:
+        current_cards.remove(11)
+        current_cards.append(1)
+    return sum(current_cards)
+
 
 player_hand = []
 dealer_hand = []
@@ -19,26 +31,18 @@ for draw in range(2):
     player_hand.append(deal_new_card())
     dealer_hand.append(deal_new_card())
 
+while not game_over:
+    user_score = score(player_hand)
+    dealer_score = score(dealer_hand)
 
-# def start_round():
-#     player_hand = [deal_new_card(), deal_new_card()]
-#     print(player_hand)
-#     dealer_hand = [deal_new_card()]
-#     print(dealer_hand)
-#     return player_hand, dealer_hand
-# start_round()
+    print(f"Your cards: {player_hand}, current score: {user_score}")
+    print(f"Dealer's first card: {dealer_hand[0]}")
 
-def score(current_cards):
-    if sum(current_cards) == 21 and len(current_cards) == 2:
-        print("Blackjack")
-    if sum(current_cards) > 21 and 11 in current_cards:
-        current_cards.remove(11)
-        current_cards.append(1)
-    return sum(current_cards)
-
-
-user_score = score(player_hand)
-
-print(f"Your cards: {player_hand}")
-print(f"Your score is {user_score}")
-print(f"Dealer cards: {dealer_hand}")
+    if user_score == 0 or dealer_score == 0 or user_score > 21:
+        game_over = True
+    else:
+        hit = input("Type 'y' to hit(another card) or type 'n' to pass: ")
+        if hit == 'y':
+            player_hand.append(deal_new_card())
+        else:
+            game_over = True
